e2c4781b478806238a0d3c6e1765f7dde3821f67
gigxmanshajami <mansajami2020@gmail.com>
2025-07-01 00:18:58 +0530

    updated

--- components/ui/cover.jsx
+++ components/ui/cover.jsx
@@ -35,7 +35,7 @@ export const Cover = ({
       onMouseEnter={() => setHovered(true)}
       // onMouseLeave={() => setHovered(false)}
       ref={ref}
-      className="relative hover:bg-neutral-900  group/cover inline-block dark:bg-neutral-900 bg-neutral-100 px-2 py-2  transition duration-200 rounded-sm">
+      className="relative bg-neutral-900  group/cover inline-block dark:bg-neutral-900  px-2 py-2  transition duration-200 rounded-sm">
       <AnimatePresence>
         {hovered && (
           <motion.div
@@ -89,44 +89,13 @@ export const Cover = ({
             top: `${position}px`,
           }} />
       ))}
-      <motion.span
-        key={String(hovered)}
-        animate={{
-          scale: hovered ? 0.8 : 1,
-          x: hovered ? [0, -30, 30, -30, 30, 0] : 0,
-          y: hovered ? [0, 30, -30, 30, -30, 0] : 0,
-        }}
-        exit={{
-          filter: "none",
-          scale: 1,
-          x: 0,
-          y: 0,
-        }}
-        transition={{
-          duration: 0.2,
-          x: {
-            duration: 0.2,
-            repeat: Infinity,
-            repeatType: "loop",
-          },
-          y: {
-            duration: 0.2,
-            repeat: Infinity,
-            repeatType: "loop",
-          },
-          scale: {
-            duration: 0.2,
-          },
-          filter: {
-            duration: 0.2,
-          },
-        }}
+      <span
         className={cn(
           "dark:text-white inline-block text-neutral-900 relative z-20 group-hover/cover:text-white transition duration-200",
           className
         )}>
         {children}
-      </motion.span>
+      </span>
       <CircleIcon className="absolute -right-[2px] -top-[2px]" />
       <CircleIcon className="absolute -bottom-[2px] -right-[2px]" delay={0.4} />
       <CircleIcon className="absolute -left-[2px] -top-[2px]" delay={0.8} />
